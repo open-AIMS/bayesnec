a4e05f1fe93afe657bd088a90d0aa7fb4d237634
Diego Barneche <diego.barneche@ymail.com>
2020-05-29 11:11:06 +0800

    fixed documentation basic typos, added github actions and pkgdown structure

---
+++ bayesnec.Rcheck/00_pkg_src/bayesnec/R/check_data.R
@@ -0,0 +1,175 @@
+#' check_data
+#'
+#' Check data input for a bayesian nec model fit
+#'
+#' @param  data a data.frame containing the data to use for the model
+#' 
+#' @param x_var the column heading indicating the concentration (x) variable
+#' 
+#' @param y_var the column heading indicating the response (y) variable
+#' 
+#' @param trials_var the column heading indicating the column for the number of "trials" for binomial response data. 
+#' If not supplied, the model may run but will not be the model you intended!
+#' 
+#' @param x_type the statistical distribution to use for the x (concentration) data. This will be guess based on the 
+#' characteristic of the input data if not supplied.
+#'
+#' @param y_type the statistical distribution to use for the y (response) data. This may currently be one of  'binomial', 
+#' 'poisson',' 'gaussian', or 'gamma'. Others can be added as required, please contact the package maintainer. 
+#' If not supplied, the appropriate distribution will be guessed based on the distribution of the input data.
+#'
+#' @param over_disp If an overdispersed model should be used. Only changes the model fit for poisson and binomial y_type 
+#' data. For poisson, a negative binomial model will be fit. For binomial a beta model will be fit.
+#'
+#' @param model The type of model to be fit. Currently takes values of "nec3param",  
+#' "nec4param", "necsigmoidal", "nechorme", "ecx4param", "ecxwb1", or "ecxwb2".
+#' 
+#' @details   
+#' 
+#' This is a wrapper function to test input data criteria and write the brms model file for use in a bayesnec model fit
+#'
+#' @export
+#' @return Modified elements of the bayesnec input data.
+
+check_data <- function(data,
+            x_var,
+            y_var,
+            trials_var,
+            x_type = NA, 
+            y_type = NA,
+            over_disp,
+            model){
+  
+  if (!is.na(y_type)) {
+    if (over_disp & y_type == "beta") {
+      y_type <- NA
+    }
+  }
+  
+  # check the specified columns exist in data
+  use_vars <- na.omit(c(y_var = y_var, x_var = x_var, trials_var))
+  var_colms <- match(use_vars, colnames(data))
+  missing_colms <- data.frame(val = use_vars[which(is.na(var_colms))], stringsAsFactors = FALSE)
+  missing_colms$element <- rownames(missing_colms)
+  if (length(na.omit(var_colms)) < length(use_vars)) {
+    stop(paste0("Your indicated ", paste(paste0(missing_colms$element, " '", missing_colms$val,"'"),
+                                        collapse = ", "),
+               " is not present in your input data. Has this been mispecified?"))
+  }
+  
+  # extract the data
+  y_dat <- data[, y_var]
+  x_dat <- data[, x_var]
+  
+  # check the x data are numeric
+  if (!inherits(x_dat, "numeric")) {
+    stop(paste0("Your indicated x_var column ", x_var," contains data that is class ", class(x_dat),".
+                 The function bayesnec requires the concentration data (argument x_var) to be numeric."))
+  }
+  
+  # check data contains only finite values
+  test_x <- mean(x_dat)
+  test_y <- mean(y_dat)
+  if (!is.finite(test_x)) {
+    stop("Your x_var column contains values that are not finite.")
+  }
+  if (!is.finite(test_y)) {
+    stop("Your y_var column contains values that are not finite.")
+  }
+  
+  # check the data are lower at high x compared to low x (ie the response variable declines within increase in the x)
+  resp_check <- mean(y_dat[which(x_dat < mean(x_dat))]) < mean(y_dat[which(x_dat > mean(x_dat))])
+  if (resp_check & model != "nechorme") {
+    stop("The mean value of the response for the lower half of the
+           concentration data are lower than that of the upper half of the concentration data.
+           bayesnec only fits concentration response data where the
+           response declines with increasing values of concentration.")
+  }
+  
+  # check variable type x_var
+  if (is.na(x_type)) {
+    x_type <- set_distribution(x_dat)
+  }
+  if (is.na(y_type)) {
+    y_type <- set_distribution(y_dat, support_integer = TRUE,
+                               trials = data[, trials_var])
+  }
+  
+  # check there is a valid model type
+  if (!model %in% c("nec3param", "necsigm", "nec4param", "nechorme",
+                    "ecx4param", "ecxwb1", "ecxwb2", "ecxlin", "ecxexp", "ecxsigm")) {
+    stop("The model type you have specified does not exist.")
+  }
+  
+  if (y_type == "poisson" & over_disp) {
+    y_type <- "negbin"
+  }
+  if (y_type == "binomial" & over_disp) {
+    y_type <- "beta"
+    data[,y_var] <-  data[, y_var] / data[, trials_var]
+  }
+  
+  # error catching for 0 for gamma by adding very small value
+  if (min(data[,x_var])==0 & x_type=="gamma"){
+    tt <- data[,x_var]
+    min_val <- min(tt[which(tt>0)])
+    data[which(tt==0),x_var] <- tt[which(tt==0)]+(min_val/10)
+  }
+  
+  if (min(data[,y_var])==0 & y_type=="gamma"){
+    tt <- data[,y_var]
+    min_val <- min(tt[which(tt>0)])
+    data[which(tt==0),y_var] <- tt[which(tt==0)]+(min_val/10)
+  }
+  # error catching for 0 for beta by adding very small value (beta does not take zero)
+  if (min(data[,x_var])==0 & x_type=="beta"){
+    tt <- data[,x_var]
+    min_val <- min(tt[which(tt>0)])
+    data[which(tt==0),x_var] <- tt[which(tt==0)]+(min_val/10)
+  }
+  
+  if (min(data[,y_var])==0 & y_type=="beta"){
+    tt <- data[,y_var]
+    min_val <- min(tt[which(tt>0)])
+    data[which(tt==0),y_var] <- tt[which(tt==0)]+(min_val/10)
+  }
+  
+  # error catching for 1 for beta by subtracting very small value (beta does not take 1)
+  if (max(data[,x_var])==1 & x_type=="beta"){
+    tt <- data[,x_var]
+    data[which(tt==1),x_var] <- tt[which(tt==1)]-0.001
+  }
+  
+  if (max(data[,y_var])==1 & y_type=="beta"){
+    tt <- data[,y_var]
+    data[which(tt==1),y_var] <- tt[which(tt==1)]-0.001
+  }
+  
+  # create brms model data
+  mod_dat <<- data.frame(x = data[,x_var],   # concentration
+                         y = data[,y_var], # response (successes)
+                         N = nrow(data))  # Sample size
+  
+  response <- data[, y_var]
+  
+  if (y_type == "binomial") {
+    mod_dat$trials <- data[, trials_var] # number of "trials"
+    response <- data[, y_var] / data[, trials_var]
+  }
+
+  mod_file <- define_model(model=model, x_type=x_type, y_type=y_type, mod_dat=mod_dat)
+  bform <- mod_file$bform
+  priors <- mod_file$priors
+  mod_family <- mod_file$mod_family
+    
+  list(priors = priors,
+       response = response,
+       mod_dat = mod_dat,
+       data = data,
+       y_type = y_type,
+       x_type = x_type,
+       x_dat = x_dat,
+       y_dat = y_dat,
+       bform = bform,
+       mod_family = mod_family)
+}
---
+++ bayesnec.Rcheck/00_pkg_src/bayesnec/R/fit_bayesnec.R
@@ -0,0 +1,195 @@
+#' fit_bayesnec
+#'
+#' Fits a concentration(dose)-response model using brms
+#'
+#' @param data a data.frame containing the data to use for the model fit
+#' @param x_var the column heading indicating the concentration (x) variable
+#' @param y_var the column heading indicating the response (y) variable
+#' @param trials_var the column heading indicating the column for the number of "trials" for binomial response data. 
+#' If not supplied, the model may run but will not be the model you intended!
+#'#' @param x_type the statistical distribution to use for the x (concentration) data. This will be guess based on the 
+#' characteristic of the input data if not supplied.
+#' @param y_type the statistical distribution to use for the y (response) data. This may currently be one of  'binomial', 
+#' 'poisson',' 'gaussian', or 'gamma'. Others can be added as required, please contact the package maintainer. 
+#' If not supplied, the appropriate distribution will be guessed based on the distribution of the input data.
+#' @param over_disp If an overdispersed model should be used. Only changes the model fit for poisson and binomial y_type 
+#' data. For poisson, a negative binomial model is used. For binomial a beta model is used.
+#' @param model The type of model to be fit. Currently takes values of "nec3param",  
+#' "nec4param", "necsigm", "nechorme", "ecx4param", "ecxwb1", "ecxexp", "ecxlin", or "ecxwb2".
+#' @param sig_val Probability value to use as the lower quantile to test significance of the predictor posterior values
+#' against the control, to estimate nec as an interpolated NOEC value from smooth ecx curves.
+#' @param x_range The range of x values over which to obtain posterior predictions. Used for plot.bayesnecfit and to calculate nec for ecx models.
+#' @param precision The length of the x_seq to pass to posterior_predict as new data. Used for plot.bayesnecfit and to calculate nec for ecx models.
+#' @param iter the number of interations for the brms fit. Defaults to 2e4.
+#' @param warmup the number of warmup iterations. Defaults to 4/5ths of iter.
+#' @param ... further arguments to be passed to \code{\link[brms]{brm}}.
+#' 
+#' @details   
+#' 
+#' As some concentration-response data will use zero concentration which can cause numerical estimation issues, a small offset 
+#' is added (1/10th of the next lowest value) to zero values of concentration where x_var are gamma distributed.
+#' 
+#' All models provide an estimate for nec. For model types with "nec" as a prefix, nec is directly estimated as a paremeter 
+#' in the model. Models with "ecx" as a prefix are continuous curve models, tyipically used for extracting ecx values 
+#' from concentration response data. In this instance the nec value is defined as the concentration at which there is 
+#' a user supplied (see sig_val) percentage certainty (based on the Bayesian posterior estimate) that the response falls below the estimated value of
+#' the upper assymptote (top) of the response (i.e the response value is significantly lower than that expected in the case of
+#' no exposure).
+#'
+#' @export
+#' @importFrom brms fixef brm posterior_predict loo waic
+#' @return The fitted brms model, including an estimate of the nec value and predicted posterior values.
+#' A posterior sample of the nec is also available under $nec_posterior
+
+fit_bayesnec <- function(data, x_var, y_var, trials_var = NA,
+                         x_type = NA, y_type = NA, x_range = NA, precision = 1000,
+                         over_disp = FALSE, model = "nec3param", sig_val = 0.025, iter = 2e4,
+                         warmup = floor(iter/5)*4, 
+                          ...) {
+  if(class(data)== "bayesmanecfit"){
+    response <- data$data[, y_var]
+    mod_dat <- data$mod_dat
+    #data <- data$data
+       
+    y_dat <- data$y_dat
+    x_dat <- data$x_dat   
+    y_type <- data$y_type
+    x_type <- data$x_type
+  
+    if (y_type == "binomial") {
+      response <- response / data$data[, trials_var]
+
+    }
+
+    mod_file <- define_model(model = model, x_type = x_type,
+                             y_type = y_type, mod_dat = mod_dat)
+    bform <- mod_file$bform
+    priors <- mod_file$priors
+    mod_family <- mod_file$mod_family
+    
+  } else {
+    data_check <- check_data(data = data, x_var = x_var, y_var = y_var,
+                             trials_var = trials_var, x_type = x_type,
+                             y_type = y_type,
+                             over_disp = over_disp, model = model)
+    mod_dat <- data_check$mod_dat
+    y_type <- data_check$y_type
+    x_type <- data_check$x_type
+    response <- data_check$response
+    data <- data_check$data
+    x_dat <- data_check$x_dat
+    y_dat <- data_check$y_dat 
+    init_fun <- data_check$init_fun
+    bform <- data_check$bform
+    priors <- data_check$priors  
+    mod_family <- data_check$mod_family
+  }
+  
+  fit <- brms::brm(bform, data = mod_dat, prior = priors, iter=iter,
+                       family = mod_family, warmup = warmup, refresh=0)
+  
+  fit$loo <- brms::loo(fit)
+  fit$waic <- brms::waic(fit)
+
+  out <- list(fit = fit, mod_dat = mod_dat,
+              y_type = y_type, x_type = x_type, model = model)
+
+  # extract the relevant model parameters
+  extract_params <- c("top", "beta", "nec", "alpha", "bot", "d", "slope", "ec50")
+  extracted_params <- lapply(extract_params, function(x, model_fit) {
+    fef <- fixef(model_fit)
+    tt <- fef[grep(x, rownames(fef)), c("Estimate", "Q2.5", "Q97.5")]
+    if (is.na(tt["Estimate"])) {
+      NA
+    } else {
+      tt
+    }
+  }, model_fit = fit)
+  names(extracted_params) <- extract_params
+
+  top <- extracted_params$top
+  beta <- extracted_params$beta
+  nec <- extracted_params$nec
+  alpha <- extracted_params$alpha
+  bot <- extracted_params$bot
+  d <- extracted_params$d
+  slope <- extracted_params$slope
+  ec50 <- extracted_params$ec50 
+  
+
+  if(is.na(extracted_params$nec["Estimate"])){mod_class <- "ecx"}else{mod_class <- "nec"}
+  
+  if(is.na(x_range)){
+      x_seq <- seq(min(mod_dat$x), max(mod_dat$x), length=precision)
+  }
+
+  new_dat <- data.frame(x=x_seq)
+  if(y_type=="binomial"){new_dat$trials=10^3}
+  
+  y_pred_m <- predict(fit, newdata = new_dat, robust = TRUE, re_formula = NA)
+  predicted_y <- predict(fit, robust = TRUE, re_formula = NA)
+
+  if(y_type=="binomial"){
+    top <- top/10^3
+    predicted_y <- predicted_y/10^3
+    y_pred_m <-  y_pred_m/10^3
+  }
+  
+  # calculate the residuals
+  residuals <-  response - predicted_y 
+  
+  # entire posterior
+  pred_posterior <- t(predict(fit, newdata = new_dat, re_formula = NA, summary = FALSE))
+  if(y_type=="binomial"){
+    pred_posterior <- pred_posterior/10^3
+  }
+  
+  # calculate the predicted values using the entire posterior
+  pred_vals <- c(list(x=x_seq, y=y_pred_m[,"Estimate"], up=y_pred_m[,"Q97.5"], lw=y_pred_m[,"Q2.5"],
+                      posterior=pred_posterior), list(y_pred_m=y_pred_m[,"Estimate"]))  
+
+  # Extract the overdispersion estimate
+  od <- dispersion(fit, summary = TRUE) 
+  if(is.null(od)){od <- c(NA, NA, NA, NA)}
+  
+  # get the posterior nec 
+  
+  # calculate the nec from the predicted values for the ecx model
+  if(mod_class=="ecx"){
+    reference <-  quantile(pred_vals$posterior[1, ], sig_val)
+    nec_posterior  <-  sapply(1:ncol(pred_vals$posterior), function (x, pred_vals, reference) {
+      pred_vals$x[which.min(abs(pred_vals$posterior[, x] - reference))]
+      }, 
+      pred_vals = pred_vals, reference = reference)
+    
+    nec <- quantile(nec_posterior, c(0.5, 0.025,  0.975))  
+    names(nec) <- c("Estimate", "Q2.5", "Q97.5")
+  }else{
+    nec_posterior <- unlist(posterior_samples(fit, pars="nec_Intercept"))
+  }
+  
+  # Put everyting in a list for output
+  if(class(out)!="try-error"){
+    out <- c(out, list(
+      pred_vals = pred_vals,
+      nec = nec,
+      top = top,
+      beta = beta,
+      alpha = alpha,
+      bot = bot,
+      d = d,
+      ec50 = ec50,
+      over_disp=od,
+      predicted_y = predicted_y,
+      residuals = residuals,
+      nec_posterior = nec_posterior))
+    
+    # assign a class to the output
+    class(out) <- "bayesnecfit"
+  }
+  
+  message(paste("Response variable ", y_var, " modelled as a ", model ," model using a ", y_type, " distribution.", sep=""))
+  return(out)    
+}
+
+
