1d149603920147a1141e0a36a2b5507ff93068f9
Diego Barneche <diego.barneche@ymail.com>
2020-05-31 07:39:32 +0800

    fix major bug. Close #10

--- R/ecxexp_stan.R
+++ R/ecxexp_stan.R
@@ -11,7 +11,7 @@
 #' @importFrom brms prior_summary
 ecxexp_stan <- function(new_priors = NULL, new_data = NULL, ...) {
   ecxexp_brms$fit@stanmodel <- stanmodels$ecxexp
-  if (is.null(new_priors) | is.null(new_data)) {
+  if (!is.null(new_priors) | !is.null(new_data)) {
     new_model_str <- alter_model(ecxexp_brms, new_priors = new_priors,
                                  new_data = new_data)
     ecxexp_brms <- new_model_str$brmodel
