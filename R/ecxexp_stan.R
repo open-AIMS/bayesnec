876f270af8914624bde4085c9c2efa13f8fb6092
Diego Barneche <diego.barneche@ymail.com>
2020-05-31 07:33:14 +0800

    new priors and new data are now possible. Close #10

--- R/ecxexp_stan.R
+++ R/ecxexp_stan.R
@@ -1,12 +1,25 @@
-#' Bayesian regression with Stan
+#' Fits model ECX_EXP
 #'
-#' @export
-#' @param data Numeric vector of input values.
-#' @param ... Arguments passed to `brms::brm` (e.g. iter, chains).
-#' @return An object of class `brmsfit` returned by `brms::brm`
+#' @param new_priors An object of class \code{\link[brms]{brmsprior}}.
+#' @param new_data A \code{\link[base]{data.frame}}.
+#' @param ... Arguments passed to \code{\link[brms]{update}}
+#' (e.g. iter, chains).
+#' @return An object of class \code{\link[brms]{brmsfit}} returned
+#' by \code{\link[brms]{update}}
+#' @seealso \code{\link{alter_model}}
 #' @importFrom stats update
-#'
-ecxexp_stan <- function(data, ...) {
+#' @importFrom brms prior_summary
+ecxexp_stan <- function(new_priors = NULL, new_data = NULL, ...) {
   ecxexp_brms$fit@stanmodel <- stanmodels$ecxexp
-  update(ecxexp_brms, ...)
+  if (is.null(new_priors) | is.null(new_data)) {
+    new_model_str <- alter_model(ecxexp_brms, new_priors = new_priors,
+                                 new_data = new_data)
+    ecxexp_brms <- new_model_str$brmodel
+    brpriors <- new_model_str$brpriors
+    brdata <- new_model_str$brdata
+  } else {
+    brpriors <- prior_summary(ecxexp_brms)
+    brdata <- ecxexp_brms$data
+  }
+  update(ecxexp_brms, newdata = brdata, prior = brpriors, ...)
 }
