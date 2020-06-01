30aa698305264411c85320a0f1a9cbd8a798801e
Diego Barneche <diego.barneche@ymail.com>
2020-06-01 19:09:49 +0800

    generalised stan fitting function; added all families to ecxexp. contributes to issues #11 and #12

---
+++ R/fit_stan.R
@@ -0,0 +1,30 @@
+#' Fits brm models based on model type and distribution family
+#'
+#' @param model A \code{\link[base]{character}} vector indicating the model(s)
+#' to fit.
+#' @param y_type The statistical distribution to use for the y (response) data.
+#' @param new_priors An object of class \code{\link[brms]{brmsprior}}.
+#' @param new_data A \code{\link[base]{data.frame}}.
+#' @param ... Arguments passed to \code{\link[brms]{update}}
+#' (e.g. iter, chains).
+#' @return An object of class \code{\link[brms]{brmsfit}} returned
+#' by \code{\link[brms]{update}}
+#' @seealso \code{\link{alter_model}}
+#' @importFrom stats update
+#' @importFrom brms prior_summary
+fit_stan <- function(model, y_type, new_priors = NULL, new_data = NULL, ...) {
+  brms_fit <- get(paste0(model, "_brms_", y_type))
+  stan_mod <- paste0(model, y_type)
+  brms_fit$fit@stanmodel <- stanmodels[[stan_mod]]
+  if (!is.null(new_priors) | !is.null(new_data)) {
+    new_model_str <- alter_model(brms_fit, new_priors = new_priors,
+                                 new_data = new_data)
+    brms_fit <- new_model_str$brmodel
+    brpriors <- new_model_str$brpriors
+    brdata <- new_model_str$brdata
+  } else {
+    brpriors <- prior_summary(brms_fit)
+    brdata <- brms_fit$data
+  }
+  update(brms_fit, newdata = brdata, prior = brpriors, ...)
+}
--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -1,7 +1,9 @@
 library(brms)
 
-# standard package model candidates
-mod_candidates <- list(nec = c("nec3param", "nec4param", "nechorme", "necsigm"),
+#######################
+# ACCEPTED MODEL GROUPS
+#######################
+mod_groups <- list(nec = c("nec3param", "nec4param", "nechorme", "necsigm"),
                        ecx = c("ecx4param", "ecxlin", "ecxexp", "ecxsimg",
                                "ecxwb1", "ecxwb2"),
                        all = c("nec3param", "nec4param", "nechorme", "necsigm",
@@ -11,25 +13,71 @@ mod_candidates <- list(nec = c("nec3param", "nec4param", "nechorme", "necsigm"),
                                     "ecxlin", "ecxexp", "ecxsigm"))
 
 empty_stanmodel <- function() {
-	x <- ""
-	class(x) <- "stanmodel"
-	x
+  x <- ""
+  class(x) <- "stanmodel"
+  x
 }
 
+fams_loop <- function(mod_fams, bf_binom, bf_deflt,
+                      priors, data_beta, data, ...) {
+  for (i in seq_along(mod_fams)) {
+    fam_i <- names(mod_fams)[i]
+    out_s <- paste0("inst/stan/ecxexp", fam_i,
+                    ".stan")
+    if (fam_i == "binom") {
+      nf_i <- bf_binom
+    } else {
+      nf_i <- bf_deflt
+    }
+    if (fam_i == "beta") {
+      df_i <- data_beta
+    } else {
+      df_i <- data
+    }
+    mod_i <- brms::brm(nf_i, data = df_i,
+                       family = get(mod_fams[i])(),
+                       prior = priors, chains = 0,
+                       save_model = out_s)
+    mod_i$fit@stanmodel <- empty_stanmodel()
+    assign(paste0("ecxexp_brms_", fam_i), mod_i, ...)
+  }
+}
+
+##################################
+# SET MOCK DATA AND DEFAULT MODELS
+##################################
+mod_fams <- c(gaussian = "gaussian",
+              gamma = "Gamma",
+              poisson = "poisson",
+              negbin = "negbinomial",
+              binom = "binomial",
+              beta = "Beta")
+
 data <- data.frame(x = log(c(6.25, 100, 200, 50, 12.5)),
-	               y = c(93, 81, 19, 91, 57),
+                   y = c(93, 81, 19, 91, 57),
                    trials = c(99, 111, 100, 95, 102))
 
-priors <- c(prior_string("gamma(0.0001, 0.0001)", nlpar = "beta"),
-	        prior_string("normal(2, 100)", nlpar = "top"))
+data_beta <- data.frame(x = log(c(6.25, 100, 200, 50, 12.5)),
+                        y = c(0.1, 0.2, 0.5, 0.12, 0.7))
 
-bform <- brms::bf(y | trials(trials) ~ top * exp(-beta * x),
-                top + beta ~ 1,
-                nl = TRUE)
+###############
+# ECXEXP MODELS
+###############
+ecxexp_priors <- c(prior_string("gamma(0.0001, 0.0001)", nlpar = "beta"),
+                   prior_string("normal(2, 100)", nlpar = "top"))
 
-ecxexp_brms <- brms::brm(bform, data = data, family = binomial(),
-                         prior = priors, chains = 0)
+bf_ecxexp_binom <- brms::bf(y | trials(trials) ~ top * exp(-beta * x),
+                         top + beta ~ 1,
+                         nl = TRUE)
 
-ecxexp_brms$fit@stanmodel <- empty_stanmodel()
+bf_ecxexp_deflt <- brms::bf(y ~ top * exp(-beta * x),
+                            top + beta ~ 1,
+                            nl = TRUE)
 
-save(mod_candidates, ecxexp_brms, file = "R/sysdata.rda")
+fams_loop(mod_fams, bf_ecxexp_binom, bf_ecxexp_deflt,
+          ecxexp_priors, data_beta, data, envir = environment())
+
+save(mod_groups, mod_fams,
+     ecxexp_brms_gaussian, ecxexp_brms_gamma, ecxexp_brms_poisson,
+     ecxexp_brms_negbin, ecxexp_brms_binom, ecxexp_brms_beta,
+     file = "R/sysdata.rda")
