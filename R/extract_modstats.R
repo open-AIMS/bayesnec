47738dcaaa00cf4aa2de12d2fc99338466c1b592
beckyfisher <rebeccafisher76@gmail.com>
2020-06-03 16:24:42 +0800

    minor fixes #9

--- R/extract_modstats.R
+++ R/extract_modstats.R
@@ -3,7 +3,7 @@
 #' Extracts a range of statistics from a list of bayesnecfit model fits.
 #'
 #' @param mod_fits a bayesmanecfit mod_fits output list, as returned by
-#' bayesnec when more than one model is supplied.
+#' \code{\link{bnec}} when more than one model is supplied.
 #'
 #' @export
 #' @return A list of model statistical output derived from the input model list
