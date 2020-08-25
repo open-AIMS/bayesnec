45a8f2535803916102f466baae9a3c47b7ef5cd5
Diego Barneche <diego.barneche@ymail.com>
2020-08-25 13:04:18 +0800

    added stan_like (though more constrained) capability to make_inits based on priors

--- R/make_inits.R
+++ R/make_inits.R
@@ -4,15 +4,21 @@
 #'
 #' @param priors an object of class "brmsprior" from package \pkg{brms}.
 #' @param chains Number of chains to be passed to brms model.
+#' @param stan_like Should initial values be drawn in a similar
+#' fashion to how they are drawn by stan? Defaults to FALSE.
 #'
 #' @seealso \code{\link{bnec}}
 #' @return A \code{\link[base]{list}} containing the initialisation values.
-make_inits <- function(priors, chains) {
-  #set.seed(10)
+make_inits <- function(priors, chains, stan_like = FALSE) {
   fcts <- c(gamma = rgamma,
             normal = rnorm,
             beta = rbeta,
             uniform = runif)
+  fcts_st <- c(gamma = function() runif(1, 0, 2),
+               normal = function() runif(1, -2, 2),
+               beta = function() runif(1, 0, 1),
+               uniform = function() runif(1, -2, 2))
+
   priors <- as.data.frame(priors)
   priors <- priors[priors$prior != "", ]
   par_names <- character(length = nrow(priors))
@@ -31,7 +37,11 @@ make_inits <- function(priors, chains) {
       fct_i <- bits[1]
       v1 <- as.numeric(bits[2])
       v2 <- as.numeric(bits[4])
-      out[[i]][[j]] <- fcts[[fct_i]](1, v1, v2)
+      if (stan_like) {
+        out[[i]][[j]] <- fcts_st[[fct_i]]()
+      } else {
+        out[[i]][[j]] <- fcts[[fct_i]](1, v1, v2)
+      }
       if (priors$class[j] == "b") {
         dim(out[[i]][[j]]) <- 1
       }
