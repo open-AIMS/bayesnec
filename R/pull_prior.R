e4c152a23b0062597eceea23eaf881c920d1e3ec
beckyfisher <rebeccafisher76@gmail.com>
2020-08-31 09:13:08 +0800

    Added pull_prior

---
+++ R/pull_prior.R
@@ -0,0 +1,22 @@
+#' pull_prior
+#'
+#' Extracts the priors from an object of class
+#' \code{\link{bayesnecfit}} or \code{\link{bayesmanecfit}}.
+#'
+#' @param object An object of class \code{\link{bayesnecfit}} or \code{\link{bayesmanecfit}} returned by \code{\link{bnec}}.
+#' @return A list containing the priors
+#' @export
+pull_prior <- function(object) {
+  if (inherits(object, "bayesmanecfit")) {
+    mods <- object$mod_fits
+    out <- list()
+    for (i in seq_along(mods)) {
+      out[[i]] <- mods[[i]]$fit$prior
+      names(out)[i] <- mods[[i]]$model
+    }
+    out
+  } else if (inherits(object, "bayesnecfit")) {
+    object$fit$prior
+  }
+}
+pull_prior(test)
