15e2292ecce2921bcc997bfc8a2b6243f94f495c
Diego Barneche <diego.barneche@ymail.com>
2020-09-10 13:12:39 +0800

    added axample for pull_prior. contributes to #7

--- R/pull_prior.R
+++ R/pull_prior.R
@@ -3,8 +3,25 @@
 #' Extracts the priors from an object of class
 #' \code{\link{bayesnecfit}} or \code{\link{bayesmanecfit}}.
 #'
-#' @param object An object of class \code{\link{bayesnecfit}} or \code{\link{bayesmanecfit}} returned by \code{\link{bnec}}.
+#' @param object An object of class \code{\link{bayesnecfit}}
+#' or \code{\link{bayesmanecfit}} returned by \code{\link{bnec}}.
 #' @return A list containing the priors
+#'
+#' @examples
+#' \dontrun{
+#' library(tidyverse)
+#' library(brms)
+#' library(bayesnec)
+#' options(mc.cores = parallel::detectCores())
+#' data(nec_data)
+#'
+#' exmp_a <- bnec(data = nec_data, x_var = "x", y_var = "y",
+#'                model = "nec3param",
+#'                family = Beta(link = "identity"),
+#'                iter = 1e4, control = list(adapt_delta = 0.99))
+#' pull_prior(exmp_a)
+#' }
+#'
 #' @export
 pull_prior <- function(object) {
   if (inherits(object, "bayesmanecfit")) {
