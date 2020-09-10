f2f20dfec462a72c72a9eedeca2894b19165e4a4
Diego Barneche <diego.barneche@ymail.com>
2020-09-10 13:08:35 +0800

    new simulated dataset for examples

---
+++ data-raw/nec_data.R
@@ -0,0 +1,17 @@
+library(bayesnec)
+library(usethis)
+
+fct <- bayesnec:::pred_nec4param
+linear_rescale <- function(x, r_out) {
+  p <- (x - min(x)) / (max(x) - min(x))
+  r_out[[1]] + p * (r_out[[2]] - r_out[[1]])
+}
+
+set.seed(10)
+x <- abs(rnorm(100, 1))
+y <- fct(b_beta = 1.2, b_bot = 0.01, b_nec = 1.5, b_top = 0.95, x)
+y <- linear_rescale(y + rnorm(100, sd = 0.04), c(0.001, 0.98))
+
+nec_data <- data.frame(x, y)
+
+usethis::use_data(nec_data, overwrite = TRUE)
