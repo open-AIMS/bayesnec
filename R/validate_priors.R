b2bc2f728849af52325e1319f9940f8d4efdce07
Diego Barneche <diego.barneche@ymail.com>
2020-09-10 11:58:14 +0800

    priors can now be specified for few models within set

--- R/validate_priors.R
+++ R/validate_priors.R
@@ -7,16 +7,27 @@
 #' @return A \code{\link[base]{data.frame}} of class
 #' \code{\link[brms]{brmsprior}}.
 validate_priors <- function(priors, model) {
+  if (missing(priors)) {
+    return(NULL)
+  }
   if (inherits(priors, "list")) {
     if (!model %in% names(priors)) {
-      stop("Named prior list does not contain ",
-           "priors for model ", model)
+      message("Named prior list does not contain ",
+              "priors for model ", model, ".\n",
+              "Using bayesnec default priors.")
     }
     priors <- priors[[model]]
+    if (is.null(priors)) {
+      return(NULL)
+    }
   }
   if (!inherits(priors, "brmsprior")) {
-    stop("Prior is not of class brmsprior; ",
-         "see ?bnec for argument details")
+    message("Prior for model ", model,
+            " is not of class brmsprior; ",
+            "see ?bnec for argument details ",
+            "and example.\n",
+            "Using bayesnec default priors.")
+    return(NULL)
   }
   priors
 }
