11208cdd324eb5f4233afd08aaf18495d9989184
beckyfisher <rebeccafisher76@gmail.com>
2020-12-17 17:14:01 +0800

    Added models nechormepwr, nechorme4pwr, nechormepwr01

--- R/check_data.R
+++ R/check_data.R
@@ -64,7 +64,8 @@ check_data <- function(data, x_var, y_var,
   if (!model %in% c("neclin", "nec3param", "necsigm", "nec4param",
                     "nechorme", "neclinhorme", "nechorme4",
                     "ecx4param", "ecxwb1", "ecxwb2", "ecxwb1p3", "ecxwb2p3", "ecxlin",
-                    "ecxexp", "ecxsigm", "ecxll3", "ecxll4", "ecxll5", "ecxhormebc4", "ecxhormebc5")) {
+                    "ecxexp", "ecxsigm", "ecxll3", "ecxll4", "ecxll5", "ecxhormebc4", "ecxhormebc5", 
+                    "nechormepwr", "nechorme4pwr","nechormepwr01")) {
     stop(paste("The model", model, "is not a valid model name.",
                "Please check ?bnec for valid model calls."))
   }
