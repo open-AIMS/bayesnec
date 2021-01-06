9711203bf129b22eea41a2a7a0f8a0f1451c7acd
beckyfisher <rebeccafisher76@gmail.com>
2021-01-06 16:10:22 +0800

    fixed error where nechormpwr01 model was placed in the family tag

--- R/check_models.R
+++ R/check_models.R
@@ -33,7 +33,7 @@ check_models <- function(model, family) {
   }
   if (link_tag == "identity" & fam_tag %in% c("beta", "binomial", "custom")) {
     use_model <-  model[!model %in% c("neclin", "nechorme", "neclinhorme",
-                                      "nechorme4", "ecxlin", "nechormepwr", "nechorme4pwr")] 
+                                      "nechorme4", "ecxlin", "nechormepwr", "nechorme4pwr")]
     drop_model <- setdiff(model, use_model)
     if (length(drop_model) > 0) {
       message(paste("Dropping the model(s)",
@@ -49,8 +49,8 @@ check_models <- function(model, family) {
     }
   }
   if (link_tag == "identity" &
-        fam_tag %in% c("Gamma", "poisson", "negbinomial", "nechormepwr01")) {
-    use_model <-  model[!model %in% c("neclin", "neclinhorme", "ecxlin")]
+        fam_tag %in% c("Gamma", "poisson", "negbinomial")) {
+    use_model <-  model[!model %in% c("neclin", "neclinhorme", "ecxlin", "nechormepwr01")]
     drop_model <- setdiff(model, use_model)
     if (length(drop_model) > 0) {
       message(paste("Dropping the model", paste0(drop_model, collapse = ", "),
