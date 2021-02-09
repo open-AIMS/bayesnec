00df0a970830f502feb88dbc37ff5e3267afaeca
Diego Barneche <barnechedr@gmail.com>
2021-02-09 15:37:22 +0800

    fix to tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -8,13 +8,16 @@ is_on_cran <- test_that("model is always properly specified as character", {
 
 if (!is_on_cran) {
   suppress_bnec <- function(...) {
-    bayesnec::bnec(...) %>%
+    bnec(...) %>%
       suppressWarnings %>%
       suppressMessages
   }
+  logit <- function(p) {
+    log(p / (1 - p))
+  }
   # gaussian
   manec_gausian_identity <- nec_data %>%
-    dplyr::mutate(y = car::logit(y)) %>%
+    mutate(y = logit(y)) %>%
     suppress_bnec("x", "y", model = c("nec4param", "ecx4param"),
                    iter = 50, chains = 2)
   # beta
@@ -26,45 +29,45 @@ if (!is_on_cran) {
                     iter = 50, chains = 2, family = Beta(link = "identity"))
   # binomial
   manec_binomial_logit <- nec_data %>%
-    dplyr::mutate(trials = 10, y = as.integer(round(y * trials))) %>%
+    mutate(trials = 10, y = as.integer(round(y * trials))) %>%
     suppress_bnec("x", "y", trials_var = "trials",
                   model = c("nec4param", "ecx4param"), iter = 50, chains = 2)
   manec_binomial_identity <- nec_data %>%
-    dplyr::mutate(trials = 10, y = round(y * trials)) %>%
+    mutate(trials = 10, y = round(y * trials)) %>%
     suppress_bnec("x", "y", trials_var = "trials",
                   model = c("nec4param", "ecx4param"), iter = 50, chains = 2,
                   family = binomial(link = "identity"))
   # betabinomial
   manec_betabinomial <- nec_data %>%
-    dplyr::mutate(trials = 10, y = round(y * trials)) %>%
+    mutate(trials = 10, y = round(y * trials)) %>%
     suppress_bnec("x", "y", trials_var = "trials",
                   model = c("nec4param", "ecx4param"), iter = 50, chains = 2,
                   family = "beta_binomial2")
   # poisson
   manec_poisson_log <- nec_data %>%
-    dplyr::mutate(y = as.integer(round(exp(y * 3)))) %>%
+    mutate(y = as.integer(round(exp(y * 3)))) %>%
     suppress_bnec("x", "y", model = c("nec4param", "ecx4param"),
                   iter = 50, chains = 2)
   manec_poisson_identity <- nec_data %>%
-    dplyr::mutate(y = as.integer(round(exp(y * 3)))) %>%
+    mutate(y = as.integer(round(exp(y * 3)))) %>%
     suppress_bnec("x", "y", model = c("nec4param", "ecx4param"),
                   iter = 50, chains = 2, family = poisson(link = "identity"))
   # negative binomial
   manec_negbinomial_log <- nec_data %>%
-    dplyr::mutate(y = as.integer(round(exp(y * 3)))) %>%
+    mutate(y = as.integer(round(exp(y * 3)))) %>%
     suppress_bnec("x", "y", model = c("nec4param", "ecx4param"), iter = 50,
                   chains = 2, family = negbinomial)
   manec_negbinomial_identity <- nec_data %>%
-    dplyr::mutate(y = as.integer(round(exp(y * 3)))) %>%
+    mutate(y = as.integer(round(exp(y * 3)))) %>%
     suppress_bnec("x", "y", model = c("nec4param", "ecx4param"), iter = 50,
                   chains = 2, family = negbinomial(link = "identity"))
   # gamma
   manec_gamma_log <- nec_data %>%
-    dplyr::mutate(y = exp(y * 3)) %>%
+    mutate(y = exp(y * 3)) %>%
     suppress_bnec("x", "y", model = c("nec4param", "ecx4param"),
                   iter = 50, chains = 2)
   manec_gamma_identity <- nec_data %>%
-    dplyr::mutate(y = exp(y * 3)) %>%
+    mutate(y = exp(y * 3)) %>%
     suppress_bnec("x", "y", model = c("nec4param", "ecx4param"), iter = 50,
                   chains = 2, family = Gamma(link = "identity"))
 }
--- tests/testthat/test-define_prior.R
+++ tests/testthat/test-define_prior.R
@@ -20,6 +20,7 @@ test_that("model is always properly specified as character", {
   expect_error(define_prior(model = "ecx", family = gaussian(),
                             predictor = rnorm(100), response = 1:100))
   expect_s3_class(gamma_y_prior, "brmsprior")
+  skip_on_cran()
   expect_s3_class(manec_gausian_identity, "bayesmanecfit")
 })
 
