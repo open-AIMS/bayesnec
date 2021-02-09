022faa292342ee468fb4e2cabbe8176de3a121c0
Diego Barneche <barnechedr@gmail.com>
2021-02-09 16:26:30 +0800

    skip setup on CI, and added codecov yml to root

--- tests/testthat/test-define_prior.R
+++ tests/testthat/test-define_prior.R
@@ -21,6 +21,7 @@ test_that("model is always properly specified as character", {
                             predictor = rnorm(100), response = 1:100))
   expect_s3_class(gamma_y_prior, "brmsprior")
   skip_on_cran()
+  skip_on_ci()
   expect_s3_class(manec_gausian_identity, "bayesmanecfit")
 })
 
