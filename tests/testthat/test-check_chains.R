8438236e7ee2add1d714236a33afe63e3683461e
beckyfisher <rebeccafisher76@gmail.com>
2021-02-10 12:53:26 +0800

    test file for check_chains

---
+++ tests/testthat/test-check_chains.R
@@ -0,0 +1,10 @@
+library(bayesnec)
+
+necfit <- pull_out(manec_gausian_identity, "nec4param")
+
+test_that("returns null", {
+  skip_on_cran()
+  skip_on_ci()
+  expect_equal(check_chains.default(necfit), NULL)
+})
+
