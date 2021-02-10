6ed6cbffa39f824035b00f29784d5d62601782bc
Diego Barneche <barnechedr@gmail.com>
2021-02-10 14:47:13 +0800

    new architecture for tests

--- tests/testthat/test-check_chains.R
+++ tests/testthat/test-check_chains.R
@@ -1,10 +1,5 @@
 library(bayesnec)
 
-necfit <- pull_out(bayesnec:::model_fits$manec_gausian_identity, "nec4param")
-
 test_that("returns null", {
-  skip_on_cran()
-  skip_on_ci()
-  expect_equal(check_chains.default(necfit), NULL)
+  expect_equal(check_chains.default(nec_gausian_identity), NULL)
 })
-
