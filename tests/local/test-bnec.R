cb9aa8f4b65dc756acd1eb76ff6db4c2a4decf1e
beckyfisher <rebeccafisher76@gmail.com>
2021-02-11 11:48:53 +0800

    Added tests for beta with identity link

--- tests/local/test-bnec.R
+++ tests/local/test-bnec.R
@@ -10,7 +10,7 @@ test_that("gaussian model with identity works correctly", {
   expect_s3_class(manec_gausian_identity, "bayesmanecfit")
   expect_s3_class(nec_gausian_identity, "bayesnecfit")
   expect_equal(summary(manec_gausian_identity)$family, "gaussian")  
-  expect_range(manec_gausian_identity$mod_stats[1,"wi"], 0.7, 0.9)
+  expect_range(manec_gausian_identity$mod_stats[1,"wi"], 0.7, 0.95)
   expect_range(ec10[1], 1.38, 1.64)
   expect_range(ec90[1], 2.93, 2.97)
   
@@ -25,4 +25,14 @@ test_that("gaussian model with identity works correctly", {
   expect_range(fixef(fit2)["top_Intercept", 1], 2.1, 2.4)
   expect_range(fixef(fit2)["beta_Intercept", 1], 0.8, 1.0)
   expect_warning(expect_range(waic(fit2)$estimates[3, 1], 175, 215))  
+})
+
+nec_beta_identity <- pull_out(manec_beta_identity, "nec4param")
+test_that("beta model with identity works correctly", {
+  expect_s3_class(manec_beta_identity, "bayesmanecfit")
+  expect_s3_class(nec_beta_identity, "bayesnecfit")
+  expect_equal(summary(manec_beta_identity)$family, "beta")  
+  expect_range(manec_beta_identity$mod_stats[1,"wi"], 0.7, 0.99)
+  expect_range(ecx(manec_beta_identity)[1], 1.38, 1.64)
+  expect_range(ecx(manec_beta_identity, ecx_val = 90, type = "relative")[1], 2.5, 2.97)
 })
