ee1f0dafc814abaabe02199fbcd6e80b8b405bbe
beckyfisher <rebeccafisher76@gmail.com>
2021-02-11 16:25:55 +0800

    added tests for nec, ex10, ex90, family and links for all test families

--- tests/local/setup.R
+++ tests/local/setup.R
@@ -16,6 +16,7 @@ expect_range <- function(object, lower = -Inf, upper = Inf, ...) {
   expect_true(all(object >= lower & object <= upper), ...)
 }
 
+
 message("\n\n\nTemporarily caching models for local tests\n\n\n")
 
 message("\n# Gaussian\n")
@@ -25,51 +26,61 @@ manec_gausian_identity <- nec_data %>%
 
 message("\n# Beta\n")
 manec_beta_logit <- nec_data %>%
-  muted_bnec("x", "y", model = c("nec4param", "ecx4param"),
-             iter = 50, chains = 2)
+  muted_bnec("x", "y", model = c("nec4param", "ecx4param"), chains = 2)
 manec_beta_identity <- nec_data %>%
+  muted_bnec("x", "y", model = c("nec4param", "ecx4param"), chains = 2, family = Beta(link = "identity"))
+
+message("\n# Binomial\n")
+manec_binomial_logit <- nec_data %>%
+  mutate(trials = 10, y = as.integer(round(y * trials))) %>%
+  muted_bnec("x", "y", trials_var = "trials", model = c("nec4param", "ecx4param"), chains = 2)
+manec_binomial_identity <- nec_data %>%
+  mutate(trials = 10, y = round(y * trials)) %>%
+  muted_bnec("x", "y", trials_var = "trials",
+             model = c("nec4param", "ecx4param"), chains = 2,
+             family = binomial(link = "identity"))
+
+message("\n# Betabinomial\n")
+manec_betabinomial <- nec_data %>%
+  mutate(trials = 10, y = round(y * trials)) %>%
+  muted_bnec("x", "y", trials_var = "trials",
+             model = c("nec4param", "ecx4param"), chains = 2,
+             family = "beta_binomial2")
+
+message("\n# Poisson\n")
+manec_poisson_log <- nec_data %>%
+  mutate(y = as.integer(round(exp(y * 3)))) %>%
+  muted_bnec("x", "y", model = c("nec4param", "ecx4param"), chains = 2)
+manec_poisson_identity <- nec_data %>%
+  mutate(y = as.integer(round(exp(y * 3)))) %>%
   muted_bnec("x", "y", model = c("nec4param", "ecx4param"),
-             iter = 50, chains = 2, family = Beta(link = "identity"))
-# message("\n# Binomial\n")
-# manec_binomial_logit <- nec_data %>%
-#   mutate(trials = 10, y = as.integer(round(y * trials))) %>%
-#   muted_bnec("x", "y", trials_var = "trials",
-#              model = c("nec4param", "ecx4param"), iter = 50, chains = 2)
-# manec_binomial_identity <- nec_data %>%
-#   mutate(trials = 10, y = round(y * trials)) %>%
-#   muted_bnec("x", "y", trials_var = "trials",
-#              model = c("nec4param", "ecx4param"), iter = 50, chains = 2,
-#              family = binomial(link = "identity"))
-# message("\n# Betabinomial\n")
-# manec_betabinomial <- nec_data %>%
-#   mutate(trials = 10, y = round(y * trials)) %>%
-#   muted_bnec("x", "y", trials_var = "trials",
-#              model = c("nec4param", "ecx4param"), iter = 50, chains = 2,
-#              family = "beta_binomial2")
-# message("\n# Poisson\n")
-# manec_poisson_log <- nec_data %>%
-#   mutate(y = as.integer(round(exp(y * 3)))) %>%
-#   muted_bnec("x", "y", model = c("nec4param", "ecx4param"),
-#              iter = 50, chains = 2)
-# manec_poisson_identity <- nec_data %>%
-#   mutate(y = as.integer(round(exp(y * 3)))) %>%
-#   muted_bnec("x", "y", model = c("nec4param", "ecx4param"),
-#              iter = 50, chains = 2, family = poisson(link = "identity"))
-# message("\n# Negative binomial\n")
-# manec_negbinomial_log <- nec_data %>%
-#   mutate(y = as.integer(round(exp(y * 3)))) %>%
-#   muted_bnec("x", "y", model = c("nec4param", "ecx4param"), iter = 50,
-#              chains = 2, family = negbinomial)
-# manec_negbinomial_identity <- nec_data %>%
-#   mutate(y = as.integer(round(exp(y * 3)))) %>%
-#   muted_bnec("x", "y", model = c("nec4param", "ecx4param"), iter = 50,
-#              chains = 2, family = negbinomial(link = "identity"))
-# message("\n# Gamma\n")
-# manec_gamma_log <- nec_data %>%
-#   mutate(y = exp(y * 3)) %>%
-#   muted_bnec("x", "y", model = c("nec4param", "ecx4param"),
-#              iter = 50, chains = 2)
-# manec_gamma_identity <- nec_data %>%
-#   mutate(y = exp(y * 3)) %>%
-#   muted_bnec("x", "y", model = c("nec4param", "ecx4param"), iter = 50,
-#              chains = 2, family = Gamma(link = "identity"))
+             chains = 2, family = poisson(link = "identity"))
+
+message("\n# Negative binomial\n")
+manec_negbinomial_log <- nec_data %>%
+  mutate(y = as.integer(round(exp(y * 3)))) %>%
+  muted_bnec("x", "y", model = c("nec4param", "ecx4param"), chains = 2, family = negbinomial)
+manec_negbinomial_identity <- nec_data %>%
+  mutate(y = as.integer(round(exp(y * 3)))) %>%
+  muted_bnec("x", "y", model = c("nec4param", "ecx4param"), chains = 2, family = negbinomial(link = "identity"))
+
+message("\n# Gamma\n")
+manec_gamma_log <- nec_data %>%
+  mutate(y = exp(y * 3)) %>%
+  muted_bnec("x", "y", model = c("nec4param", "ecx4param"), chains = 2)
+manec_gamma_identity <- nec_data %>%
+  mutate(y = exp(y * 3)) %>%
+  muted_bnec("x", "y", model = c("nec4param", "ecx4param"), chains = 2, family = Gamma(link = "identity"))
+
+manec_fits <- list(manec_gausian_identity = manec_gausian_identity,
+                   manec_beta_logit = manec_beta_logit,
+                   manec_beta_identity = manec_beta_identity,
+                   manec_betabinomial = manec_betabinomial,
+                   manec_poisson_log = manec_poisson_log,
+                   manec_poisson_identity = manec_poisson_identity,
+                   manec_negbinomial_log = manec_negbinomial_log,
+                   manec_negbinomial_identity = manec_negbinomial_identity,
+                   manec_gamma_log = manec_gamma_log,
+                   manec_gamma_identity = manec_gamma_identity)
+
+nec_fits <- lapply(manec_fits, pull_out, model = "nec4param")
