8362f147b17679f4a3332f625a42f9106e39cb4d
beckyfisher <rebeccafisher76@gmail.com>
2021-02-12 10:48:51 +0800

    added function for randomly naming files

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,6 +2,10 @@ library(bayesnec)
 library(dplyr)
 library(testthat)
 
+random_filename <- function(nchar){
+  paste0(c(round(runif(nchar)*15), sample(letters, nchar), sample(LETTERS, nchar))[sample(1:nchar*3, nchar)], collapse="")
+}
+  
 add_na <- function(x, n = 3) {
   x_b <- x
   x_b[sample(seq_along(x), n)] <- NA
