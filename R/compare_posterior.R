794655c4352a64230811b640cb8b71dcb6d5189b
beckyfisher <rebeccafisher76@gmail.com>
2021-02-12 13:48:03 +0800

    removed redundant warning

--- R/compare_posterior.R
+++ R/compare_posterior.R
@@ -44,9 +44,7 @@
 compare_posterior <- function(x, comparison = "nec", ecx_val = 10, 
                               type = "absolute", hormesis_def = "control",
                               sig_val = 0.01, precision, x_range = NA) {
-  if (is.null(comparison)){
-    stop("You must specify a comparison type")
-    }
+  
   if (class(x) != "list" | is.null(names(x))){
     stop("Argument x must be a named list")
   }
