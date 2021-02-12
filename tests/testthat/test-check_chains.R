3349d2e62bbd29edaef420c5f05682a96d71d7df
beckyfisher <rebeccafisher76@gmail.com>
2021-02-12 11:20:40 +0800

    adde check for producing pdf file with filename

--- tests/testthat/test-check_chains.R
+++ tests/testthat/test-check_chains.R
@@ -3,3 +3,10 @@ library(bayesnec)
 test_that("returns null", {
   expect_null(check_chains.default(nec_gausian_identity))
 })
+
+test_that("returns pdf for baysmanec objects with filename argument", {
+  filename <- random_filename(15)
+  expect_invisible(check_chains(manec_gausian_identity, filename = filename))
+  on.exit(file.remove(paste(filename, ".pdf", sep = ""))) 
+})
+
