8925cebef995eb1dc8eb680daee058344cc359bf
beckyfisher <rebeccafisher76@gmail.com>
2021-02-17 10:27:20 +0800

    Added tests for dispersion

---
+++ tests/testthat/test-dispersion.R
@@ -0,0 +1,11 @@
+
+test_fam <- nec4param$fit
+test_fam$family$family <- "other"
+
+test_that("dispersion works", {
+  expect_gt(length(bayesnec:::dispersion(nec4param$fit)), 3)
+  expect_length(bayesnec:::dispersion(nec4param$fit, summary = TRUE), 3)
+  expect_null(bayesnec:::dispersion(test_fam))
+  expect_type(bayesnec:::dispersion(nec4param$fit), "double")
+  expect_type(bayesnec:::dispersion(nec4param$fit, summary = TRUE), "double")  
+})
