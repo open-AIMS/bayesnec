dd6db88c9772f95de47a759b7caa14b818e2de53
beckyfisher <rebeccafisher76@gmail.com>
2021-03-11 21:11:49 +0800

    Added functio nec including tests

---
+++ R/nec.R
@@ -0,0 +1,133 @@
+#' nec.default
+#'
+#' Extracts the predicted nec value as desired from an object of class
+#' \code{\link{bayesnecfit}} or \code{\link{bayesnecfit}}.
+#'
+#' @param object An object of class \code{\link{bayesnecfit}} or
+#' \code{\link{bayesmanecfit}} returned by \code{\link{bnec}}.
+#' @param posterior A \code{\link[base]{logical}} value indicating if the full
+#' posterior sample of calculated nec values should be returned instead of
+#' just the median and 95 credible intervals.
+#' @param xform A function to apply to the returned estimated concentration
+#' values.
+#' @param prob_vals A vector indicating the probability values over which to
+#' return the estimated nec value. Defaults to 0.5 (median) and 0.025 and
+#' 0.975 (95 percent credible intervals).
+#'
+#' @seealso \code{\link{bnec}}
+#'
+#' @return A vector containing the estimated nec value, including upper and
+#' lower 95% credible interval bounds (or other interval as specified by prob_vals).
+#'
+#' @importFrom stats quantile predict
+#'
+#' @examples
+#' \dontrun{
+#' library(brms)
+#' library(bayesnec)
+#' options(mc.cores = parallel::detectCores())
+#' data(nec_data)
+#'
+#' exmp <- bnec(data = nec_data, x_var = "x", y_var = "y",
+#'              model = c("nec4param", "ecx4param"),
+#'              family = Beta(link = "identity"), priors = my_priors,
+#'              iter = 1e4, control = list(adapt_delta = 0.99))
+#' exmp_2 <- pull_out(exmp, "ecx4param")
+#' nec(exmp)
+#' nec(exmp_2)
+#' }
+#'
+#' @export
+nec.default <- function(object, posterior = FALSE,  xform = NA,
+                         prob_vals = c(0.5, 0.025, 0.975)) {
+  if(length(prob_vals)<3 | prob_vals[1]<prob_vals[1] | prob_vals[1]>prob_vals[3] | prob_vals[2]>prob_vals[3]){
+    stop("prob_vals must include central, lower and upper quantiles, in that order")
+  }
+  if (length(grep("ecx", object$model)) > 0) {
+    mod_class <- "ecx"
+  } else {
+    mod_class <- "nec"
+  }
+  
+  if (mod_class == "ecx") {
+    stop("nec is not a parameter in ecx model types.")
+  }
+
+  nec_out <- object$nec_posterior
+  if (inherits(xform, "function")) {
+    nec_out <- xform(nec_out)
+  }
+  nec_estimate <- quantile(unlist(nec_out), probs = prob_vals)
+  
+  if (!posterior) {
+    nec_estimate
+  } else {
+    nec_out
+  }
+}
+
+#' nec
+#'
+#' Extracts the predicted nec value as desired from an object of class
+#' \code{\link{bayesnecfit}} or \code{\link{bayesnecfit}}.
+#'
+#' @inheritParams nec.default
+#'
+#' @param object An object of class \code{\link{bayesnecfit}} or
+#' \code{\link{bayesnecfit}} returned by \code{\link{bnec}}.
+#'
+#' @inherit nec.default return details seealso examples
+#'
+#' @export
+nec <- function(object, posterior = FALSE,
+                 xform = NA, prob_vals = c(0.5, 0.025, 0.975)) {
+  UseMethod("nec")
+}
+
+#' nec.bayesnecfit
+#'
+#' Extracts the predicted nec value as desired from an object of class
+#' \code{\link{bayesnecfit}}.
+#'
+#' @param object An object of class \code{\link{bayesnecfit}}
+#' returned by \code{\link{bnec}}.
+#' @param ... Additional arguments to \code{\link{nec}}
+#'
+#' @inherit nec return details seealso examples
+#' @export
+nec.bayesnecfit <- function(object, ...) {
+  nec.default(object, ...)
+}
+
+#' nec.bayesmanecfit
+#'
+#' Extracts the predicted nec value as desired from an object of class
+#' \code{\link{bayesmanecfit}}.
+#'
+#' @inheritParams nec
+#'
+#' @param object An object of class \code{\link{bayesmanecfit}} returned by
+#' \code{\link{bnec}}.
+#'
+#' @inherit nec return details seealso examples
+#'
+#' @importFrom stats quantile
+#'
+#' @export
+nec.bayesmanecfit <- function(object, posterior = FALSE, x_range = NA,
+                               xform = NA, prob_vals = c(0.5, 0.025, 0.975)) {
+  if(max(grepl("ecx", names(object$mod_fits)))==1) {
+    warning("Bayesmanecfit contains ecx model types and therefore nec estimate includes nsec values.")
+  }
+  nec_out <- object$w_nec_posterior
+  if (inherits(xform, "function")) {
+    nec_out <- xform(nec_out)
+  }
+  nec_estimate <- quantile(unlist(nec_out), probs = prob_vals)
+  
+  if (!posterior) {
+    nec_estimate
+  } else {
+    nec_out
+  }
+}
---
+++ tests/testthat/test-nec.R
@@ -0,0 +1,41 @@
+library(bayesnec)
+library(brms)
+
+
+test_that("nec returns expected object types", {
+  nec_summary <- nec(manec_gausian_identity)
+  expect_equal(length(nec_summary), 3)
+})
+
+test_that("doesn't work for ecx models", {
+  expect_error(nec(ecx4param), "nec is not a parameter in ecx model types.") 
+})
+  
+test_that("works for bayesnecfit", {
+  nec1 <- nec(nec4param) 
+  expect_equal(length(nec1), 3)  
+  expect_equal(names(nec1), c("50%", "2.5%", "97.5%"))
+})
+
+test_that("works for bayesmanecfit", {
+  nec1 <- expect_warning(nec(manec_gausian_identity), "Bayesmanecfit contains ecx model types and therefore nec estimate includes nsec values.") 
+  expect_equal(length(nec1), 3)  
+  expect_equal(names(nec1), c("50%", "2.5%", "97.5%"))
+})
+
+
+test_that("xform passes correctly", {
+  nec1 <- nec(nec4param) 
+  nec2 <- nec(nec4param, xform=exp) 
+  expect_gt(nec2[1], nec1[2])
+})
+
+test_that("posterior passes correctly", {
+  nec3 <- nec(nec4param, posterior = TRUE)   
+  expect_equal(length(nec3), 10)  
+})
+
+test_that("prob_vals passes correctly", {
+  nec4 <- nec(nec4param, prob_vals = c(0.3, 0.5, 0.7))   
+  expect_equal(names(nec4), c("30%", "50%", "70%"))
+})  
