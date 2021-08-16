907d190cc9ab18fbfe4d28c9c63c6d3e42319cbb
Diego Barneche <diego.barneche@ymail.com>
2021-08-16 13:30:59 +0800

    small fixes to tests and documentation

--- tests/testthat/test-bnec.R
+++ tests/testthat/test-bnec.R
@@ -9,7 +9,12 @@ nec_data <- nec_data %>%
          trials = as.integer(20))
 
 test_that("Check for data when using formula syntax", {
-  expect_error(bnec(y ~ model(x, "ecxlin")), " You must supply data if using formula syntax.")
-  expect_error(bnec(x=nec_data$x, y=nec_data$count, model="ecxlin", trials_var = "trials"),
-             "If data are passed using x, trials_var must contain a numeric vector indicating the number of trials for each observation")
+  expect_error(bnec(y ~ model(x, "ecxlin")), "You must supply data if using formula syntax.")
+  expect_warning(
+    expect_error(
+      bnec(x = nec_data$x, y = nec_data$count, model = "ecxlin",
+           trials_var = "trials"),
+      "If data are passed using x, trials_var must contain a numeric vector indicating the number of trials for each observation."
+    )
+  )
 })
