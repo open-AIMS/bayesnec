861c0067213e956541a7e4bfaf48e61abc12c5df
beckyfisher <rebeccafisher76@gmail.com>
2021-08-20 15:53:46 +0800

    corrections to tests to accomodate default identity link

--- tests/testthat/test-check_models.R
+++ tests/testthat/test-check_models.R
@@ -1,25 +1,28 @@
 library(bayesnec)
 
 test_that("properly drops zero bounded models for logit and log links", {
-  beta_family_default <- validate_family("Beta")
-  binomial_family_default <- validate_family("binomial")
-  poisson_family_default <- validate_family("poisson")
-  negbinomial_family_default <- validate_family("negbinomial")
-  gamma_family_default <- validate_family("Gamma")
+  beta_family_logit <- validate_family(Beta(link="logit"))
+  binomial_family_logit <- validate_family(binomial(link = "logit"))
+  poisson_family_log <- validate_family(poisson(link = "log"))
+  negbinomial_family_log <- validate_family(negbinomial(link = "log"))
+  gamma_family_log <- validate_family(Gamma(link = "log"))
   gaussian_family_default <- validate_family("gaussian")
+  
   expect_equal(check_models(c("nec3param", "nec4param", "ecxexp"),
-                   beta_family_default), "nec4param")
+                   beta_family_logit), "nec4param")
   expect_equal(check_models(c("nec3param", "ecx4param", "ecxexp"),
-                   beta_family_default), "ecx4param")
+                   beta_family_logit), "ecx4param")
   expect_equal(check_models(c("nec3param", "ecx4param", "ecxexp"),
-                   poisson_family_default), "ecx4param")
+                   poisson_family_log), "ecx4param")
   expect_equal(check_models(c("nec3param", "ecx4param", "ecxexp"),
-                   beta_family_default),  "ecx4param")
-})
+                   beta_family_logit),  "ecx4param")
+
+  
+  })
 
 test_that("when all models dropped an error is returned", {
-  beta_family_default <- validate_family("Beta")
-  expect_error(check_models(c("nec3param", "ecxexp"), beta_family_default))
+  beta_family_logit <- validate_family(Beta(link = "logit"))
+  expect_error(check_models(c("nec3param", "ecxexp"), beta_family_logit))
 })
 
 test_that(paste0("properly drops lin models for identity link for",
@@ -29,6 +32,7 @@ test_that(paste0("properly drops lin models for identity link for",
   poisson_family_identity <- validate_family(poisson(link = "identity"))
   negbin_family_identity <- validate_family(negbinomial(link = "identity"))
   gamma_family_identity <- validate_family(Gamma(link = "identity"))
+  
   expect_error(check_models(c("neclin", "neclinhorme", "ecxlin"),
                    beta_family_identity))
   expect_error(check_models(c("neclin", "neclinhorme", "ecxlin"),
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -7,7 +7,7 @@ test_that("nec_data are as expected", {
 
 test_that("beta_binomial2 family behaves as expected", {
   expect_equal(class(beta_binomial2), c("customfamily", "brmsfamily", "family"))
-  expect_equal(beta_binomial2$link, "logit")
+  expect_equal(beta_binomial2$link, "identity")
   expect_equal(beta_binomial2$link_phi, "log")
   expect_equal(beta_binomial2$dpars, c("mu", "phi"))
 })
