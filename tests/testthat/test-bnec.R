91051b95b7a42e76da9e74b2d5bcc1ec3b74464a
beckyfisher <rebeccafisher76@gmail.com>
2021-08-20 14:49:37 +0800

    Further error catching, including test

--- tests/testthat/test-bnec.R
+++ tests/testthat/test-bnec.R
@@ -6,7 +6,8 @@ other_data <- nec_data
 colnames(other_data) <- c("a", "b")
 nec_data <- nec_data %>% 
   mutate(count = as.integer(round(y*20)),
-         trials = as.integer(20))
+         trials = as.integer(20),
+         log.x = log(x))
 
 test_that("Check for data when using formula syntax", {
   expect_error(bnec(y ~ model(x, "ecxlin")), "You must supply data if using formula syntax.")
@@ -17,4 +18,11 @@ test_that("Check for data when using formula syntax", {
       "If data are passed using x, trials_var must contain a numeric vector indicating the number of trials for each observation."
     )
   )
+})
+
+test_that("Check models inappropriate for negative x are dropped", {
+ bnec(data=nec_data, x_var = "log.x", y_var = "y", model = "nechorme4pwr") %>% 
+    expect_message("Dropping the model\\(s\\) nechorme4pwr as they are not valid for data with negative predictor \\(x\\) values\\.")  %>%  
+    expect_error("No valid models have been supplied for this data type.") 
+
 })
