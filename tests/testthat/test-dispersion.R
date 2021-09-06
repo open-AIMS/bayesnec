8993cc3372355377517e5ecc148c6c7ac5a49edb
Diego Barneche <barnechedr@gmail.com>
2021-09-06 14:27:15 +0800

    self-containing tests for dispersion; avoids CRAN Windows issue

--- tests/testthat/test-dispersion.R
+++ tests/testthat/test-dispersion.R
@@ -3,21 +3,20 @@ library(bayesnec)
 data(manec_example)
 
 data(nec_data)
-nec_data$y <- as.integer(round(nec_data$y * 100))
-nec4param <- bnec(y ~ crf(x, "nec4param"), data = nec_data, chains = 2) %>%
-  suppressMessages %>%
-  suppressWarnings
-
-ecx4param <- pull_out(manec_example, model = "ecx4param") %>%
-  suppressMessages %>%
-  suppressWarnings
 
 test_that("dispersion fails because family is gaussian", {
+  ecx4param <- pull_out(manec_example, model = "ecx4param") %>%
+    suppressMessages %>%
+    suppressWarnings
   expect_length(dispersion(ecx4param), 0)
   expect_length(dispersion(ecx4param, summary = TRUE), 0)
 })
 
 test_that("dispersion works because family is binomial", {
+  nec_data$y <- as.integer(round(nec_data$y * 100))
+  nec4param <- bnec(y ~ crf(x, "nec4param"), data = nec_data, chains = 2) %>%
+    suppressMessages %>%
+    suppressWarnings
   expect_length(dispersion(nec4param), 4e3)
   expect_length(dispersion(nec4param, summary = TRUE), 3)
   expect_type(dispersion(nec4param), "double")
