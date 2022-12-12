91c02bcd50801de7efd0aadda03673a1c485e3a8
Diego Barneche <barnechedr@gmail.com>
2022-12-12 21:14:48 +0800

    fixed inconsistency in generic pull_brms arguments across methods; contributes to #108

--- R/pull_brmsfit.R
+++ R/pull_brmsfit.R
@@ -3,8 +3,7 @@
 #'
 #' @param object An object of class \code{\link{bayesnecfit}} or
 #' \code{\link{bayesmanecfit}} returned by \code{\link{bnec}}.
-#' @param model An optional \code{\link[base]{character}} vector specifying the 
-#' model to extract in the case of a \code{\link{bayesmanecfit}}. 
+#' @param ... Arguments passed to other methods.
 #'
 #' @seealso \code{\link{bnec}}
 #'
@@ -16,7 +15,7 @@
 #' brms_fit <- pull_brmsfit(manec_example, model = "nec4param")
 #'
 #' @export
-pull_brmsfit <- function(object, model = NA) {
+pull_brmsfit <- function(object, ...) {
   UseMethod("pull_brmsfit")
 }
 
@@ -33,7 +32,7 @@ pull_brmsfit <- function(object, model = NA) {
 #' @noRd
 #'
 #' @export
-pull_brmsfit.bayesnecfit <- function(object) {
+pull_brmsfit.bayesnecfit <- function(object, ...) {
   object$fit
 }
 
@@ -44,23 +43,24 @@ pull_brmsfit.bayesnecfit <- function(object) {
 #'
 #' @param object An object of class \code{\link{bayesmanecfit}} returned by
 #' \code{\link{bnec}}.
+#' @param model An optional \code{\link[base]{character}} vector specifying
+#' the model to extract.
+#' @param ... Unused.
 #'
 #' @inherit pull_brmsfit examples return
 #'
 #' @noRd
 #'
 #' @export
-pull_brmsfit.bayesmanecfit <- function(object, model = NA) {
+pull_brmsfit.bayesmanecfit <- function(object, model = NA, ...) {
   models <- names(object$mod_fits)
   if (is.na(model)) {
-    stop(paste("Input is a bayesmanecfit, containing the models ",
-          paste(models, collapse = ", "), ". 
-          You must specify the model to pull.",
-          sep=""))
+    stop("Input is a bayesmanecfit, containing the models ",
+         paste0(models, collapse = ", "), ". You must specify the model",
+         " to pull.")
   }
-  if(model %in% models == FALSE){
-    stop(paste ("Model must be one of ", paste(models, collapse = ", ")))
+  if (!(model %in% models)) {
+    stop("Model must be one of ", paste(models, collapse = ", "))
   }
   object$mod_fits[[model]]$fit
-
 }
