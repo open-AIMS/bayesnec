f2aaa32eaa713c7b0b949460e0351ee3a3899556
Diego Barneche <barnechedr@gmail.com>
2022-12-12 21:25:03 +0800

    updated NAMESPACE, tests and vignettes following recent changes

--- tests/testthat/test-bayesnec_methods.R
+++ tests/testthat/test-bayesnec_methods.R
@@ -7,9 +7,13 @@ test_that("predict is silent", {
 
 test_that("predict output is a list of appropriately name elements", {
   pred_p <- predict(nec4param)
-  expect_equal(class(pred_p), "list")
-  expect_equal(length(pred_p), 2)
-  expect_equal(names(pred_p), c("data", "posterior"))
+  expect_equal(class(pred_p), c("matrix", "array"))
+  expect_equal(dim(pred_p), c(100, 4))
+  expect_equal(colnames(pred_p), c("Estimate", "Est.Error", "Q2.5", "Q97.5"))
+  fitt_p <- fitted(nec4param)
+  expect_equal(class(fitt_p), c("matrix", "array"))
+  expect_equal(dim(fitt_p), c(100, 4))
+  expect_equal(colnames(fitt_p), c("Estimate", "Est.Error", "Q2.5", "Q97.5"))
 })
 
 test_that("plot returns null, is invisible, and is silent", {
@@ -19,8 +23,7 @@ test_that("plot returns null, is invisible, and is silent", {
 })
 
 test_that("summary behaves as expected", {
-  summary_p <- summary(nec4param) %>%
-    suppressWarnings
+  summary_p <- suppressWarnings(summary(nec4param))
   expect_equal(class(summary_p), "necsummary")
   expect_equal(names(summary_p), c("brmssummary", "model", "is_ecx", "ecs",
                                    "bayesr2"))
--- tests/testthat/test-nec.R
+++ tests/testthat/test-nec.R
@@ -4,8 +4,7 @@ library(brms)
 data(manec_example)
 
 test_that("nec returns expected object types", {
-  nec_summary <- nec(manec_example) %>%
-    suppressMessages
+  nec_summary <- suppressMessages(nec(manec_example))
   expect_equal(length(nec_summary), 3)
 })
 
@@ -22,8 +21,7 @@ test_that("works for bayesnecfit", {
 test_that("works for bayesmanecfit", {
   mes <- paste0("bayesmanecfit contains ecx model types and therefore nec",
                 " estimate includes nsec values.")
-  nec1 <- nec(manec_example) %>%
-    suppressMessages
+  nec1 <- suppressMessages(nec(manec_example))
   expect_message(nec(manec_example), mes)
   expect_equal(length(nec1), 3)
   expect_equal(names(nec1), c("50%", "2.5%", "97.5%"))
