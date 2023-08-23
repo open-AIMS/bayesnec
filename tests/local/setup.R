2109ef97dc826ab776f41419e6e9ee8017fe7fc8
Rebecca Fisher <r.fisher@aims.gov.au>
2023-08-23 13:21:39 +0800

    Revert "Added nsec method and tests for brmsfit"
    This reverts commit c928eb1c155958bf5981801a4cfb83df73c86e00.

--- tests/local/setup.R
+++ tests/local/setup.R
@@ -152,24 +152,3 @@ nec_data <- nec_data |>
 a <- nec_data$x
 b <- nec_data$y
 j <- nec_data$trials
-
-data(herbicide)
-herbicide <- herbicide |> 
-  dplyr::mutate(x = log(concentration),
-                y = (fvfm+0.001)*0.999)
-fit <-  bnec(y ~ crf(x, model = "ecx4param"), data = herbicide, 
-             family = Beta(), seed = 17)
-
-fit_brms <- pull_brmsfit(fit)
-
-priors.fit <- prior_summary(fit_brms)
-priors.fit
-bf_fitInt <- brms::bf(y ~ top + (bot - top)/(1 + exp((ec50 - x) * exp(beta))),
-                      top + bot + beta + ec50 ~ herbicide,
-                      nl = TRUE)
-fit_brmsInt <- brm(bf_fitInt, data = herbicide, family = Beta(),
-                   prior = priors.fit, iter = 5000,  save_pars = save_pars(all=TRUE),
-                   seed = 700, init = 0)
-
-
-
